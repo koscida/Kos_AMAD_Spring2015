3f0b829c97b20d6a547f0cc75daa39eb35d73b54
Brittany Ann Kos <britkos@gmail.com>
2016-03-30 03:22:52 -0600

    adding folders

--- IGNORE_working_labs/ios_labs/Kos_Lab_8/Pods/Target Support Files/Pods-classwork_03_17/Pods-classwork_03_17-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_classwork_03_17VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_classwork_03_17VersionString[];
-
--- IGNORE_working_labs/ios_labs/classwork_03_17/Pods/Target Support Files/Pods-classwork_03_17/Pods-classwork_03_17-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_classwork_03_17 : NSObject
-@end
-@implementation PodsDummy_Pods_classwork_03_17
-@end
